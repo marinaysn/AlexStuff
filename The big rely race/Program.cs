4e85dd5f8a50a7452941f3b2718df29b81b44e1c
marinaysn <marinaysn@gmail.com>
2016-12-26 20:12:05 -0500

    first commit

--- The big rely race/Program.cs
+++ The big rely race/Program.cs
@@ -11,6 +11,46 @@ namespace The_big_rely_race
         static void Main(string[] args)
         {
 
+            int age = 0;
+            string name = "John";
+            decimal height = 0.00M;
+            float weight = 0.00f;
+            string favorFood = "";
+            string favorColor;
+
+
+            Console.WriteLine("Hi user\n\nWhat is your name?");
+
+            name = Console.ReadLine();
+
+            Console.WriteLine("\n\nHi " + name + "\n\nNice meeting you");
+            Console.WriteLine("How old are you?");
+
+            age = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("\n\nThank you " + name + " You are " + age);
+
+            Console.WriteLine("\n\nWhat is your height in ft?(if perfect int please add .0");
+            height = Decimal.Parse(Console.ReadLine());
+
+            Console.WriteLine("\n\n height " + height + " ,ok thank you for sharing your height");
+
+            Console.WriteLine("Can you please tell us your weight?");
+
+            weight = float.Parse(Console.ReadLine());
+
+            Console.WriteLine("Nice, you weigh " + weight);
+
+
+
+
+
+
+
+
+
+
+            Console.ReadLine();
 
         }
     }
