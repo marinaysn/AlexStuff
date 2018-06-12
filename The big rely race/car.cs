d0ba75f91c4ebc38a6ec78c9c5d39d17e39456fd
Marina Ysnogorodsky <marinaysn@gmail.com>
2018-06-11 22:33:45 -0400

    added class car

---
+++ The big rely race/car.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace The_big_rely_race
+{
+    class car
+    {
+        int num = 10;
+
+    }
+}
